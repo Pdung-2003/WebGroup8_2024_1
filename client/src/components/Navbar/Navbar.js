06bbb038432e4038410af02231c8d29af8795bfa
duowngng <duowng.ng@gmail.com>
2024-10-23 22:08:04 +0700

    feat: add Navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,28 @@
 import { NavLink } from "react-router-dom";
+import { BiUserCircle, BiSearch } from "react-icons/bi"
+import { RiArrowDropDownFill } from "react-icons/ri"
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-      </ul>
+      <div className="left">
+        <div className="logo">LOGO</div>
+        <div className='searchbox'>
+          <BiSearch className='searchbtn' />
+          <input type="text" placeholder="Search For a Movie" />
+        </div>
+      </div>
+      <div className="right">
+        <p className='dropdown'>
+          Choose Location
+          <RiArrowDropDownFill className="dropicon" />
+        </p>
+        <NavLink to="/" className='theme_btn1 linkstylenone'>Logout</NavLink>
+        <NavLink to="/" className='linkstylenone'>
+          <BiUserCircle className='theme_icon1' />
+        </NavLink>
+      </div>
     </nav>
   );
 };
