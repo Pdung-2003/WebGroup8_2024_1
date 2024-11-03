87cf240d1ca2ba4ad91a2aec3e5aa7ae2508b4f9
duowngng <duowng.ng@gmail.com>
2024-11-03 09:33:40 +0700

    feat: authentication pages (Sign In & Sign Up)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import NavBar from "./components/Navbar/Navbar";
 import Home from './pages/Home';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
 import About from './pages/About';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
 import './App.css';
@@ -11,6 +13,8 @@ const App = () => {
         <NavBar />
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/auth/signin" element={<SignIn />} />
+            <Route path="/auth/signup" element={<SignUp />} />
             <Route path="/about" element={<About />} />
             <Route path="/movies/:id" element={<MovieDetails />} />
         </Routes>
