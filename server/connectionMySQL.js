3ab432c093bb87b2720ca6b4530bcd2e28b5ce7e
DuongDH03 <duong.dh215020@sis.hust.edu.vn>
2024-11-03 12:09:31 +0700

    Fixed the database file, and rm .env from server

--- server/connectionMySQL.js
+++ server/connectionMySQL.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost', // Địa chỉ MySQL server
-    user: 'root',      // Tên đăng nhập MySQL
-    password: 'your_password',  // Mật khẩu MySQL
-    database: 'cinema_booking'  // Tên database
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,    
+    password: process.env.DB_PASSWORD,  
+    database: process.env.DB_DATABASE  
 });
 
 connection.connect((err) => {
