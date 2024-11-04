1fe4b3d00203e5d2ddd22a03bbd2b49e8579d3f5
Pdung2003 <dung.pd20215265@sis.hust.edu.vn>
2024-11-05 02:25:43 +0700

    using sequelize in DB

---
+++ server/model/User.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../connection');
+
+const User = sequelize.define('User', {
+    user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    tel: { type: DataTypes.STRING(15) },
+    password: { type: DataTypes.STRING(40), allowNull: false }
+}, {
+    tableName: 'User',
+    timestamps: false
+});
+
+module.exports = User;
