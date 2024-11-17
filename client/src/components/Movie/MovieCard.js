57614cbbe3726e0460a7b27357f379eaf2b04170
DuongHoang673 <hoangducduong03kjzg@gmail.com>
2024-11-17 23:44:40 +0700

    feat:Trang Home va trang Payment

--- client/src/components/Movie/MovieCard.js
+++ client/src/components/Movie/MovieCard.js
@@ -4,6 +4,7 @@ import { BsFillStarFill } from "react-icons/bs";
 import "./MovieCard.css";
 
 const MovieCard = (data) => {
+  const genres = Array.isArray(data?.genres) ? data.genres.join(", ") : "No genres available";
   const navigate = useNavigate();
   const { title, imageUrl, _id, rating, genre } = data;
 
@@ -28,7 +29,7 @@ const MovieCard = (data) => {
       </div>
       <div className="details">
         <p className="title">{title}</p>
-        <p className="genre">{genre.join(", ")}</p>
+        <p className="genre">{genres}</p>
       </div>
     </div>
   );
