a01efc033f6958183bbb3c4e455df25aa620dada
DuongDH03 <duong.dh215020@sis.hust.edu.vn>
2024-11-19 22:31:38 +0700

    fix: Movie Details, added function to fetch

--- client/src/pages/MovieDetails/MovieDetails.js
+++ client/src/pages/MovieDetails/MovieDetails.js
@@ -1,45 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchMovieByName, fetchMovieById } from "../../function/movie";
 import MovieSchedule from "./MovieSchedule";
 import "./MovieDetails.css";
 
 const MovieDetails = () => {
-  const movie = {
-    wideposter:
-      "https://bhdstar.vn/wp-content/uploads/2024/10/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp-17.jpg",
-    portraitposter:
-      "https://bhdstar.vn/wp-content/uploads/2024/10/referenceSchemeHeadOfficeallowPlaceHoldertrueheight700ldapp-21.jpg", // No separate portrait poster found, using the same image
-    title: "VENOM: THE LAST DANCE: KÈO CUỐI",
-    ageLimit: "T13",
-    type: "Phụ đề",
-    formats: ["2D", "3D"],
-    duration: "109 phút",
-    genre: "Action",
-    releasedate: "25/10/2024",
-    cast: [
-      {
-        _id: "1",
-        name: "Tom Hardy",
-        role: "Actor",
-        imageUrl: "https://bhdstar.vn/dien-vien/tom-hardy/", // Placeholder actor image URL
-      },
-      {
-        _id: "2",
-        name: "Juno Pemple",
-        role: "Actor",
-        imageUrl: "https://bhdstar.vn/dien-vien/juno-pemple/", // Placeholder actor image URL
-      },
-    ],
-    crew: [
-      {
-        _id: "1",
-        name: "Kelly Marcel",
-        role: "Director",
-        imageUrl: "https://bhdstar.vn/dao-dien/kelly-marcel/", // Placeholder director image URL
-      },
-    ],
-    about:
-      "Eddie và Venom đang chạy trốn. Bị cả hai thế giới truy đuổi, bộ đôi buộc phải đưa ra một quyết định tàn khốc để hạ màn cho điệu nhảy cuối cùng của họ.",
-  };
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      const result = await fetchMovieById(6);
+      if (result.success) {
+        setMovie(result.movie);
+      } else {
+        console.error(result.error);
+      }
+    };
+
+    getMovie();
+  }, []);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="movie-page container bg-base-100">
@@ -116,7 +98,7 @@ const MovieDetails = () => {
               <iframe
                 width="640"
                 height="360"
-                src="https://www.youtube.com/embed/iwaU3a_zD2o?si=CBVAzO0OPj-EbgfA"
+                src={movie.trailer_url}
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
