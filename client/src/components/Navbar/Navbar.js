928d8c9ce24fee558aac4303c8d16818031f4d5c
DuongHoang673 <hoangducduong03kjzg@gmail.com>
2024-11-25 22:17:30 +0700

    profile va sua navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { isSignedIn } from "../../function/auth";
 import logo from "../../assets/logo.png";
+import { Dropdown, Menu, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
   const [signedIn, setSignedIn] = useState(isSignedIn());
@@ -13,10 +15,10 @@ const Navbar = () => {
 
   useEffect(() => {
     const scrollHandler = () => {
-      window.scrollY > 10 ? setTop(false) : setTop(true)
+      window.scrollY > 10 ? setTop(false) : setTop(true);
     };
-    window.addEventListener('scroll', scrollHandler);
-    return () => window.removeEventListener('scroll', scrollHandler);
+    window.addEventListener("scroll", scrollHandler);
+    return () => window.removeEventListener("scroll", scrollHandler);
   }, [top]);
 
   const handleLogout = () => {
@@ -25,39 +27,52 @@ const Navbar = () => {
     window.location.href = "/";
   };
 
+  // Menu for dropdown
+  const menu = (
+    <Menu>
+      <Menu.Item key="1">
+        <a href="/profile">Thông tin cá nhân</a>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <a href="/settings">Cài đặt</a>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3" danger onClick={handleLogout}>
+        Đăng xuất
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <header className={`sticky top-0 z-50 ${!top && "drop-shadow-md"}`}>
       <nav className="border-2">
-        <div className="container-fluid bg-base-100 md:px-24">
+        <div className="container bg-base-100 px-24">
           <div className="navbar">
             <div className="navbar-start">
               <a href="/" className="flex flex-row items-center justify-start">
                 <div className="w-8 md:w-12">
                   <img src={logo} alt="Logo" className="img" />
                 </div>
-                <div className="mx-1 be-vietnam-pro-black tracking-widest text-xl md:text-3xl">CINEMAX</div>
+                <div className="mx-1 be-vietnam-pro-black tracking-widest text-3xl md:text-3xl">CINEMAX</div>
               </a>
             </div>
             <div className="navbar-end">
               {signedIn ? (
                 <>
-                  <div className="avatar">
-                    <div className="w-7 md:w-9 rounded-full">
-                      <img
-                        src="https://bhdstar.vn/wp-content/assets/loodo/no-user.jpg"
-                        alt="Avatar"
-                      />
+                <Dropdown overlay={menu} trigger={["click"]}>
+                  <Space className="cursor-pointer">
+                    <div className="avatar">
+                      <div className="w-9 rounded-full">
+                        <img
+                          src="https://bhdstar.vn/wp-content/assets/loodo/no-user.jpg"
+                          alt="Avatar"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <span className="mx-2 text-xs md:text-lg">Username</span>
-                  <span>|</span>
-                  <a
-                    href="/auth/signin"
-                    className="mx-2 text-sm md:text-lg be-vietnam-pro-bold"
-                    onClick={handleLogout}
-                  >
-                    Đăng xuất
-                  </a>
+                    <span>Username</span>
+                    <DownOutlined />
+                  </Space>
+                </Dropdown>
                 </>
               ) : (
                 <a
