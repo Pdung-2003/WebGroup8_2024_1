69f08ecdd444e372597b1486048ff98c2f14ebcb
duowngng <duowng.ng@gmail.com>
2024-11-28 21:26:50 +0700

    fix: update home slider

--- client/src/components/HomeSlider/HomeSlider.jsx
+++ client/src/components/HomeSlider/HomeSlider.jsx
@@ -1,59 +1,64 @@
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import { Keyboard, Mousewheel, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Keyboard, Mousewheel, Navigation, Pagination } from "swiper/modules";
+import { fetchFeaturedMovies } from "../../function/movie";
 
 export const HomeSlider = () => {
-  const banners = [
-    {
-      imgUrl:
-        "https://bhdstar.vn/wp-content/uploads/2024/11/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp-19.jpg",
-    },
-    {
-      imgUrl:
-        "https://bhdstar.vn/wp-content/uploads/2024/11/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp-18.jpg",
-    },
-    {
-      imgUrl:
-        "https://bhdstar.vn/wp-content/uploads/2024/11/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp-8.jpg",
-    },
-    {
-      imgUrl:
-        "https://bhdstar.vn/wp-content/uploads/2024/11/referenceSchemeHeadOfficeallowPlaceHoldertrueheight1069ldapp-2.jpg",
-    },
-  ];
+  const [featuredMovies, setFeaturedMovies] = useState([]);
+
+  useEffect(() => {
+    const getFeaturedMovies = async () => {
+      const result = await fetchFeaturedMovies();
+      if (result.success) {
+        setFeaturedMovies(result.movies.map(movie => ({
+          title: movie.title,
+          wide_poster_url: movie.wide_poster_url,
+          movie_id: movie.movie_id,
+        })));
+        console.log(result);
+      } else {
+        console.error(result.error);
+      }
+    };
+
+    getFeaturedMovies();
+  }, []);
 
   return (
     <Swiper
       cssMode={true}
       loop={true}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
       pagination={{
         clickable: true,
       }}
       navigation={true}
       mousewheel={true}
       keyboard={true}
-      modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+      modules={[Autoplay, Navigation, Pagination, Mousewheel, Keyboard]}
       className="mySwiper"
     >
-      {banners.map((banner, index) => {
-        return (
-          <SwiperSlide key={index}>
-            <div className="relative w-full h-[300px] md:h-[400px] lg:h-[600px]">
-              <a href="/movies/1">
-                <img
-                  src={banner.imgUrl}
-                  alt=""
-                  className="w-full h-full object-cover"
-                />
-              </a>
-            </div>
-          </SwiperSlide>
-        );
-      })}
+      {featuredMovies.map((movie) => (
+        <SwiperSlide key={movie.movie_id}>
+          <div className="relative w-full h-[300px] md:h-[400px] lg:h-[600px]">
+            <a href={`/movies/${movie.movie_id}`}>
+              <img
+                src={movie.wide_poster_url}
+                alt={movie.title || "Featured Movie"}
+                className="w-full h-full object-cover"
+              />
+            </a>
+          </div>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
